a51dfcb95ba5267fb30002d25dd4920686449f11
João Victor <joaovictorramalho7@gmail.com>
2021-07-20 12:35:07 -0300

    Create categories routes

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+
+const categoriesRoutes = Router();
+
+const categories = [];
+
+categoriesRoutes.post("/categories", (request, response) => {
+  const { name, description } = request.body;
+
+  categories.push({
+    name,
+    description,
+  });
+
+  return response.sendStatus(201);
+});
+
+export { categoriesRoutes };
