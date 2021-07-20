3ae59f76b946bc48b8b579141d5af71035aa4fb5
João Victor <joaovictorramalho7@gmail.com>
2021-07-20 12:34:37 -0300

    Import and use categories routes

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 app.use(express.json());
+
+app.use(categoriesRoutes);
 
 app.listen(3333, () =>
   console.log("🚀 Server is running at http://localhost:3333")
