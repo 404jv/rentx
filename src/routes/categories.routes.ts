9f420d77a2ccd9b4a3efa8407d462dc7b8eb3309
João Victor <joaovictorramalho7@gmail.com>
2021-07-21 18:49:37 -0300

    Verify if category exists before create it

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,12 @@ const categoriesRepository = new CategoryRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category Already Exists!" });
+  }
+
   categoriesRepository.create({ name, description });
 
   return response.sendStatus(201);
