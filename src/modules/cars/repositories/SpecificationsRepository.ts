3723703ed7b91d4017f7e6f1c093b420cc500357
João Victor <joaovictorramalho7@gmail.com>
2021-07-23 19:04:55 -0300

    Create specifications repository

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,35 @@
+import { Specification } from "../models/Specification";
+import {
+  ICreateSpecificationDTO,
+  ISpecificationsRepository,
+} from "./ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const specifications = new Specification();
+
+    Object.assign(specifications, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specifications);
+  }
+
+  findByName(name: string): Specification {
+    const specification = this.specifications.find(
+      (specification) => specification.name === name
+    );
+
+    return specification;
+  }
+}
+
+export { SpecificationsRepository };
