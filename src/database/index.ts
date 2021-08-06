c5e2afd6d75b2a4c55f7861460652f2ca7ff10e0
João Victor <joaovictorramalho7@gmail.com>
2021-08-06 17:21:55 -0300

    Create connection with database

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,7 @@
-import { createConnection } from "typeorm";
+import { createConnection, getConnectionOptions } from "typeorm";
 
-createConnection();
+getConnectionOptions().then((options) => {
+  createConnection({
+    ...options,
+  }).then(() => console.log("📦 Database connected."));
+});
