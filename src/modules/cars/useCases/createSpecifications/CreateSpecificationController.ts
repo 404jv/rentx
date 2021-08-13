31421b6f45de6469a27743bf804e8520c32875c8
João Victor <joaovictorramalho7@gmail.com>
2021-08-12 21:48:03 -0300

    Refactor create specification use case

--- src/modules/cars/useCases/createSpecifications/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecifications/CreateSpecificationController.ts
@@ -4,14 +4,14 @@ import { container } from "tsyringe";
 import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
 
 class CreateSpecificationController {
-  handle(request: Request, response: Response): Response {
+  async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
 
     const createSpecificationsUseCase = container.resolve(
       CreateSpecificationUseCase
     );
 
-    createSpecificationsUseCase.execute({ name, description });
+    await createSpecificationsUseCase.execute({ name, description });
 
     return response.sendStatus(201);
   }
