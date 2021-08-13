eff2d5c509129f6097f5199fe8cac8f2e33a4900
João Victor <joaovictorramalho7@gmail.com>
2021-08-12 21:11:57 -0300

    Refactor specifications routes

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 
-import { createSpecificationController } from "../modules/cars/useCases/createSpecifications";
+import { CreateSpecificationController } from "../modules/cars/useCases/createSpecifications/CreateSpecificationController";
 
 const specificationRoutes = Router();
 
-specificationRoutes.post("/", (request, response) => {
-  return createSpecificationController.handle(request, response);
-});
+const createSpecificationsController = new CreateSpecificationController();
+
+specificationRoutes.post("/", createSpecificationsController.handle);
 
 export { specificationRoutes };
