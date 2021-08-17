43fc67de75341367c2f08b7627c205bef4ea7488
João Victor <joaovictorramalho7@gmail.com>
2021-08-17 16:23:42 -0300

    Create user repository and create method

---
+++ src/modules/accounts/repositories/implementations/UserRepository.ts
@@ -0,0 +1,29 @@
+import { Repository } from "typeorm";
+
+import { ICreateUserDTO } from "../../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUserRepository } from "../IUserRepository";
+
+class UserRepository implements IUserRepository {
+  private repository: Repository<User>;
+
+  async create({
+    name,
+    email,
+    password,
+    username,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      email,
+      password,
+      username,
+      driver_license,
+    });
+
+    await this.repository.save(user);
+  }
+}
+
+export { UserRepository };
