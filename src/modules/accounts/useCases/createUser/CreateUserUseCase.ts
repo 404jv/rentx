e660bb6a6367c3c013ee10ac73aabc4acd2f5b52
João Victor <joaovictorramalho7@gmail.com>
2021-08-17 18:11:07 -0300

    Create Use Case to create a user

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUserDTO } from "../../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UserRepository")
+    private usersRepositories: IUsersRepository
+  ) {}
+
+  async execute({
+    name,
+    email,
+    password,
+    username,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    await this.usersRepositories.create({
+      name,
+      email,
+      password,
+      username,
+      driver_license,
+    });
+  }
+}
+
+export { CreateUserUseCase };
