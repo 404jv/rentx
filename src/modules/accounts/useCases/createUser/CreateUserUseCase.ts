d598ab99f625b6075cc2ad2d655b062352e14d4d
João Victor <joaovictorramalho7@gmail.com>
2021-08-18 17:24:31 -0300

    Use hash to create a password

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../../dtos/ICreateUserDTO";
@@ -16,10 +17,12 @@ class CreateUserUseCase {
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepositories.create({
       name,
       email,
-      password,
+      password: passwordHash,
       driver_license,
     });
   }
