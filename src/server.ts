26cf9f4fbe1565d4ac822757bfb06ed9862d4407
João Victor <joaovictorramalho7@gmail.com>
2021-08-21 17:33:30 -0300

    Create middleware to treat erros

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
+import "express-async-errors";
 
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -15,6 +17,21 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      error: "error",
+      message: `Internal server error: ${error}`,
+    });
+  }
+);
 
 app.listen(3333, () =>
   console.log("🚀 Server is running at http://localhost:3333")
