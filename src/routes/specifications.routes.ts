b03a212933f2a664e67393a4424de57ab18798e7
João Victor <joaovictorramalho7@gmail.com>
2021-08-21 17:32:06 -0300

    Use ensureAuthenticated middleware in specifications routes

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecifications/CreateSpecificationController";
 
 const specificationRoutes = Router();
 
 const createSpecificationsController = new CreateSpecificationController();
 
+specificationRoutes.use(ensureAuthenticated);
 specificationRoutes.post("/", createSpecificationsController.handle);
 
 export { specificationRoutes };
