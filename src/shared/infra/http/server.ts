bd00e14b9e5c44fd7864b43ed7ac38b50d0043cd
João Victor <joaovictorramalho7@gmail.com>
2021-09-14 15:18:55 -0300

    Declare next function and desable eslint for 25 line

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 
 import { AppError } from "@shared/errors/AppError";
@@ -20,18 +20,21 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.use((error: Error, request: Request, response: Response) => {
-  if (error instanceof AppError) {
-    return response.status(error.statusCode).json({
-      message: error.message,
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      error: "error",
+      message: `Internal server error: ${error}`,
     });
   }
-
-  return response.status(500).json({
-    error: "error",
-    message: `Internal server error: ${error}`,
-  });
-});
+);
 
 app.listen(3333, () =>
   console.log("🚀 Server is running at http://localhost:3333")
