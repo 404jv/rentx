9608ce266523153ad5de7852485f5e83e8b043da
João Victor <joaovictorramalho7@gmail.com>
2021-09-14 16:21:14 -0300

    Create Admin seed

---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,30 @@
+import { hash } from "bcrypt";
+import { v4 as uuidV4 } from "uuid";
+
+import createConnection from "../index";
+
+async function create() {
+  const connection = await createConnection("localhost");
+
+  const id = uuidV4();
+  const passwordHash = await hash("admin", 8);
+
+  await connection.query(
+    `INSERT 
+      INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+    VALUES(
+      '${id}', 
+      'admin', 
+      'admin@rentx.com.br', 
+      '${passwordHash}', 
+      true,
+      'now()',
+      '123'
+    )
+    `
+  );
+
+  await connection.close();
+}
+
+create().then(() => console.log("User admin created!"));
