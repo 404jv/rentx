97feecc91a3ee68a9123653cc43ee7b5ee802ea6
João Victor <joaovictorramalho7@gmail.com>
2021-09-17 17:20:10 -0300

    Create method findAvailable to find available cars

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -1,9 +1,12 @@
+import { IFindAvailableDTO } from "@modules/cars/dtos/IFindAvailableDTO";
+
 import { ICreateCarDTO } from "../dtos/ICreateCarDTO";
 import { Car } from "../infra/typeorm/entities/Car";
 
 interface ICarsRepository {
   create(data: ICreateCarDTO): Promise<Car>;
   findByLicensePlate(license_plate: string): Promise<Car>;
+  findAvailable(data: IFindAvailableDTO): Promise<Car[]>;
 }
 
 export { ICarsRepository };
