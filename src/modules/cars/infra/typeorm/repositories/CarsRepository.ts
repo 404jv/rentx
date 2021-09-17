e56ba43db05c4fb21cb3be31ed072bb717287816
João Victor <joaovictorramalho7@gmail.com>
2021-09-17 17:19:49 -0300

    Create method findAvailable to find available cars

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -1,6 +1,7 @@
 import { getRepository, Repository } from "typeorm";
 
 import { ICreateCarDTO } from "@modules/cars/dtos/ICreateCarDTO";
+import { IFindAvailableDTO } from "@modules/cars/dtos/IFindAvailableDTO";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 
 import { Car } from "../entities/Car";
@@ -40,6 +41,32 @@ class CarsRepository implements ICarsRepository {
     const car = this.repository.findOne({ license_plate });
 
     return car;
+  }
+
+  async findAvailable({
+    brand,
+    category_id,
+    name,
+  }: IFindAvailableDTO): Promise<Car[]> {
+    const carsQuery = this.repository
+      .createQueryBuilder("c")
+      .where("available = :available", { available: true });
+
+    if (brand) {
+      carsQuery.andWhere("c.brand = :brand", { brand });
+    }
+
+    if (name) {
+      carsQuery.andWhere("c.name = :name", { name });
+    }
+
+    if (category_id) {
+      carsQuery.andWhere("c.category_id = :category_id", { category_id });
+    }
+
+    const cars = await carsQuery.getMany();
+
+    return cars;
   }
 }
 
