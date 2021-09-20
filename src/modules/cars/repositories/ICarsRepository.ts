6be98538c75001d65594f72ee60db20b7839e651
João Victor <joaovictorramalho7@gmail.com>
2021-09-20 18:10:24 -0300

    Add findById in interface

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -7,6 +7,7 @@ interface ICarsRepository {
   create(data: ICreateCarDTO): Promise<Car>;
   findByLicensePlate(license_plate: string): Promise<Car>;
   findAvailable(data: IFindAvailableDTO): Promise<Car[]>;
+  findById(id: string): Promise<Car>;
 }
 
 export { ICarsRepository };
