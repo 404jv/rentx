c653489f77324621af85bc545db4c3cca36c5802
João Victor <joaovictorramalho7@gmail.com>
2021-09-21 18:57:16 -0300

    Create id column in method

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -22,6 +22,7 @@ class CarsRepository implements ICarsRepository {
     fine_amount,
     license_plate,
     specifications,
+    id,
   }: ICreateCarDTO): Promise<Car> {
     const car = this.repository.create({
       name,
@@ -32,6 +33,7 @@ class CarsRepository implements ICarsRepository {
       fine_amount,
       license_plate,
       specifications,
+      id,
     });
 
     await this.repository.save(car);
