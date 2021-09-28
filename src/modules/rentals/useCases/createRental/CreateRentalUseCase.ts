eb943bda89c9e601d85a3c8c64942aa488ce56c7
João Victor <joaovictorramalho7@gmail.com>
2021-09-28 15:05:27 -0300

    Create use case create rental

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -0,0 +1,44 @@
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { AppError } from "@shared/errors/AppError";
+
+interface IRequest {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
+
+class CreateRentalUseCase {
+  constructor(private rentalsRepository: IRentalsRepository) {}
+
+  async execute({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: IRequest): Promise<Rental> {
+    const carUnavailable = await this.rentalsRepository.findOpenRentalByCarId(
+      car_id
+    );
+
+    if (carUnavailable) {
+      throw new AppError("Car is unavailable.");
+    }
+
+    const rentalAlreadyOpenToUser =
+      await this.rentalsRepository.findOpenRentalByUserId(user_id);
+
+    if (rentalAlreadyOpenToUser) {
+      throw new AppError("There is a rental is progress for this user!");
+    }
+
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental;
+  }
+}
+
+export { CreateRentalUseCase };
