b5220b2d5edb6ee1fe89f1927cab450ad0e03c4f
João Victor <joaovictorramalho7@gmail.com>
2021-10-04 20:15:15 -0300

    Create RentalsRepository

---
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -0,0 +1,44 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+
+import { Rental } from "../entities/Rental";
+
+class RentalsRepository implements IRentalsRepository {
+  private repository: Repository<Rental>;
+
+  constructor() {
+    this.repository = getRepository(Rental);
+  }
+
+  async findOpenRentalByCarId(car_id: string): Promise<Rental> {
+    const rentalOpenByCar = await this.repository.findOne({ car_id });
+
+    return rentalOpenByCar;
+  }
+
+  async findOpenRentalByUserId(user_id: string): Promise<Rental> {
+    const rentalOpenByUser = await this.repository.findOne({ user_id });
+
+    return rentalOpenByUser;
+  }
+
+  async create({
+    car_id,
+    expected_return_date,
+    user_id,
+  }: ICreateRentalDTO): Promise<Rental> {
+    const rental = this.repository.create({
+      car_id,
+      expected_return_date,
+      user_id,
+    });
+
+    await this.repository.save(rental);
+
+    return rental;
+  }
+}
+
+export { RentalsRepository };
