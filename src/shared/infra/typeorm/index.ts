5f822c0755f61982c95db8665add335c21c18a3e
João Victor <joaovictorramalho7@gmail.com>
2021-10-05 19:06:00 -0300

    Use database rentx_test in localhost if NODE_ENV is test

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,9 +3,13 @@ import { createConnection, getConnectionOptions, Connection } from "typeorm";
 export default async (host = "database"): Promise<Connection> => {
   const defaultOption = await getConnectionOptions();
 
+  console.log(process.env.NODE_ENV);
+
   return createConnection(
     Object.assign(defaultOption, {
-      host,
+      host: process.env.NODE_ENV === "test" ? "localhost" : host,
+      database:
+        process.env.NODE_ENV === "test" ? "rentx_test" : defaultOption.database,
     })
   );
 };
