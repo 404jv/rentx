fb2385ced9da5f5ff0dd62c64bf6c447c6f14f7a
João Victor <joaovictorramalho7@gmail.com>
2021-10-08 21:03:17 -0300

    Get connection from infra/typeorm/index

--- src/modules/cars/useCases/CreateCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/CreateCategory/CreateCategoryController.spec.ts
@@ -1,13 +1,14 @@
 import { hash } from "bcrypt";
 import request from "supertest";
-import { Connection, createConnection } from "typeorm";
+import { Connection } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
 import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm";
 
 let connection: Connection;
 
-describe("Create category controller", () => {
+describe("Create Category controller", () => {
   beforeAll(async () => {
     connection = await createConnection();
     await connection.runMigrations();
@@ -44,6 +45,27 @@ describe("Create category controller", () => {
         Authorization: `Bearer ${token}`,
       });
 
-    expect(response.statusCode).toBe(201);
+    expect(response.status).toBe(201);
+  });
+
+  it("Should not be able to create a new category with name exists", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentx.com.br",
+      password: "admin",
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post("/categories")
+      .send({
+        name: "Category Supertest",
+        description: "Category Supertest",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(400);
   });
 });
