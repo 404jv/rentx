d4f53271dd1a37d43feb2af46959612882f58fcb
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2021-10-28 21:54:53 -0300

    Create DevolutionRentalUseCase

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalController.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalController.ts
@@ -1,65 +1,22 @@
-import { inject, injectable } from "tsyringe";
+import { Request, Response } from "express";
+import { container } from "tsyringe";
 
-import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
-import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
-import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
-import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
-import { AppError } from "@shared/errors/AppError";
+import { DevolutionRentalUseCase } from "./DevolutionRentalUseCase";
 
-interface IRequest {
-  rental_id: string;
-  user_id: string;
-}
-
-@injectable()
 class DevolutionRentalController {
-  constructor(
-    @inject("RentalsRepository")
-    private rentalsRepository: IRentalsRepository,
-    @inject("CarsRepository")
-    private carsRepository: ICarsRepository,
-    @inject("DayjsDateProvider")
-    private dateProvider: IDateProvider
-  ) {}
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id: user_id } = request.user;
+    const { id: rental_id } = request.params;
 
-  async execute({ rental_id, user_id }: IRequest): Promise<Rental> {
-    const rental = await this.rentalsRepository.findById(rental_id);
-    const car = await this.carsRepository.findById(rental.car_id);
-    const minimumDaily = 1;
+    const devolutionRentalUseCase = container.resolve(DevolutionRentalUseCase);
 
-    if (!rental) {
-      throw new AppError("Rental Not Found", 404);
-    }
+    const rental = await devolutionRentalUseCase.execute({
+      rental_id,
+      user_id,
+    });
 
-    const dateNow = this.dateProvider.dateNow();
-
-    let daily = this.dateProvider.compareInDays(rental.start_date, dateNow);
-
-    if (daily <= 0) {
-      daily = minimumDaily;
-    }
-
-    const delay = this.dateProvider.compareInDays(
-      dateNow,
-      rental.expected_return_date
-    );
-
-    let total = 0;
-
-    if (delay > 0) {
-      const calculateFine = delay * car.fine_amount;
-      total = calculateFine;
-    }
-
-    total += daily * car.daily_rate;
-
-    rental.end_date = dateNow;
-    rental.total = total;
-
-    await this.rentalsRepository.create(rental);
-    await this.carsRepository.updateAvailable({ id: car.id, available: true });
-
-    return rental;
+    return response.status(200).json(rental);
   }
 }
+
 export { DevolutionRentalController };
--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -1,3 +1,65 @@
-class DevolutionRentalUseCase {}
+import { inject, injectable } from "tsyringe";
 
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { AppError } from "@shared/errors/AppError";
+
+interface IRequest {
+  rental_id: string;
+  user_id: string;
+}
+
+@injectable()
+class DevolutionRentalUseCase {
+  constructor(
+    @inject("RentalsRepository")
+    private rentalsRepository: IRentalsRepository,
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository,
+    @inject("DayjsDateProvider")
+    private dateProvider: IDateProvider
+  ) {}
+
+  async execute({ rental_id, user_id }: IRequest): Promise<Rental> {
+    const rental = await this.rentalsRepository.findById(rental_id);
+    const car = await this.carsRepository.findById(rental.car_id);
+    const minimumDaily = 1;
+
+    if (!rental) {
+      throw new AppError("Rental Not Found", 404);
+    }
+
+    const dateNow = this.dateProvider.dateNow();
+
+    let daily = this.dateProvider.compareInDays(rental.start_date, dateNow);
+
+    if (daily <= 0) {
+      daily = minimumDaily;
+    }
+
+    const delay = this.dateProvider.compareInDays(
+      dateNow,
+      rental.expected_return_date
+    );
+
+    let total = 0;
+
+    if (delay > 0) {
+      const calculateFine = delay * car.fine_amount;
+      total = calculateFine;
+    }
+
+    total += daily * car.daily_rate;
+
+    rental.end_date = dateNow;
+    rental.total = total;
+
+    await this.rentalsRepository.create(rental);
+    await this.carsRepository.updateAvailable({ id: car.id, available: true });
+
+    return rental;
+  }
+}
 export { DevolutionRentalUseCase };
