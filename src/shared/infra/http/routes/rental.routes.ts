c6ecabb38fd9710a91676586318ffb197958b2a4
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2021-11-01 17:35:23 -0300

    Create route to ListRentalsByUserController

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+import { ListRentalsByUserController } from "@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserUseController";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
@@ -9,6 +10,7 @@ const rentalsRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalsRoutes.post("/", ensureAuthenticated, createRentalController.handle);
 
@@ -16,6 +18,12 @@ rentalsRoutes.post(
   "/devolution/:id",
   ensureAuthenticated,
   devolutionRentalController.handle
+);
+
+rentalsRoutes.get(
+  "/user",
+  ensureAuthenticated,
+  listRentalsByUserController.handle
 );
 
 export { rentalsRoutes };
