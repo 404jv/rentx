5c6e59290579230f261faca2c7eb32de1e8543c8
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2021-11-09 17:11:43 -0300

    Create object to values for auth config

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: ".AGx.9JwW)FESW;~",
+  expires_in_token: "15m",
+  secret_refresh_token: "L?,Vp4vAAk2CJ3Sw",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
