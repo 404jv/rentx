ad6db2732faf3059a9246de183ab1112f62514bb
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2021-11-09 17:09:43 -0300

    Add return type to upload function

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,8 +2,12 @@ import crypto from "crypto";
 import multer from "multer";
 import { resolve } from "path";
 
+interface IReturnUpload {
+  storage: multer.StorageEngine;
+}
+
 export default {
-  upload(folder: string) {
+  upload(folder: string): IReturnUpload {
     return {
       storage: multer.diskStorage({
         destination: resolve(__dirname, "..", "..", folder),
