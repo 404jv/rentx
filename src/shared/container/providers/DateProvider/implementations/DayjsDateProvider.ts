535254f63402a41f2f38650ec58726314e1cc831
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2021-11-09 17:11:08 -0300

    Add addDays method and implement

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -27,6 +27,10 @@ class DayjsDateProvider implements IDateProvider {
 
     return dayjs(endDate).diff(startDateUtc, "days");
   }
+
+  addDays(days: number): Date {
+    return dayjs().add(days, "days").toDate();
+  }
 }
 
 export { DayjsDateProvider };
