b4600a217b2746d0d8ec0bb644b9c26c11a9eb37
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-11-22 18:25:42 -0300

    Create resetPasswordUser use case

---
+++ src/modules/accounts/useCases/resetPasswordUser/ResetPasswordUserUseCase.ts
@@ -0,0 +1,54 @@
+import { hash } from "bcrypt";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { AppError } from "@shared/errors/AppError";
+
+interface IRequest {
+  password: string;
+  token: string;
+}
+
+@injectable()
+class ResetPasswordUserUseCase {
+  constructor(
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject("DayjsDateProvider")
+    private dateProvider: IDateProvider,
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ password, token }: IRequest): Promise<void> {
+    const userToken = await this.usersTokensRepository.findByRefreshToken(
+      token
+    );
+
+    if (!userToken) {
+      throw new AppError("Token Invalid!", 401);
+    }
+
+    const dateNow = this.dateProvider.dateNow();
+    const isExpiredToken = this.dateProvider.compareIfBefore(
+      userToken.expires_date,
+      dateNow
+    );
+
+    if (isExpiredToken) {
+      throw new AppError("Token expired!", 401);
+    }
+
+    const user = await this.usersRepository.findById(userToken.user_id);
+
+    user.password = await hash(password, 8);
+
+    await this.usersRepository.create(user);
+
+    await this.usersTokensRepository.deleteById(userToken.id);
+  }
+}
+
+export { ResetPasswordUserUseCase };
