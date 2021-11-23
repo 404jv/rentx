5c37e74d40165236265aa04a1b32286697631566
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-11-23 10:11:54 -0300

    Set database to localhost if it's test env else to database

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -5,7 +5,7 @@ export default async (host = "database"): Promise<Connection> => {
 
   return createConnection(
     Object.assign(defaultOption, {
-      host: "database",
+      host: process.env.NODE_ENV === "test" ? "localhost" : "database",
       database:
         process.env.NODE_ENV === "test" ? "rentx_test" : defaultOption.database,
     })
