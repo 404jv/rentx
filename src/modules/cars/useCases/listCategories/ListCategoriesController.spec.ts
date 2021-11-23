554f4c6c063daf4bd832c6ab708db6c3a1e2f45a
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-11-23 10:13:09 -0300

    Refactor tests

--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -37,7 +37,7 @@ describe("List Categories Controller", () => {
       password: "admin",
     });
 
-    const { token } = responseToken.body;
+    const { refresh_token } = responseToken.body;
 
     await request(app)
       .post("/categories")
@@ -46,7 +46,7 @@ describe("List Categories Controller", () => {
         description: "Category Supertest",
       })
       .set({
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${refresh_token}`,
       });
 
     const response = await request(app).get("/categories");
