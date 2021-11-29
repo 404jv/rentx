870bcc80654ee2b30687a482d3fcc3bc1a1e6304
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-11-29 18:02:58 -0300

    Upload image through storageProvider

--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,6 +1,10 @@
 import { inject, injectable } from "tsyringe";
 
 import { ICarImagesRepository } from "@modules/cars/repositories/ICarImagesRepository";
+import {
+  enFolder,
+  IStorageProvider,
+} from "@shared/container/providers/StorageProvider/IStorageProvider";
 
 interface IRequest {
   car_id: string;
@@ -11,12 +15,15 @@ interface IRequest {
 class UploadCarImagesUseCase {
   constructor(
     @inject("CarImagesRepository")
-    private carImagesRepository: ICarImagesRepository
+    private carImagesRepository: ICarImagesRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
     images_name.map(async (image_name) => {
       await this.carImagesRepository.create(car_id, image_name);
+      await this.storageProvider.save(image_name, enFolder.cars);
     });
   }
 }
