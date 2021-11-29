c9658f32714cdb2d282bb5c5cbbef0403b0f3a5e
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-11-29 19:33:58 -0300

    Add folder avatar and cars as local to static files

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -6,6 +6,7 @@ import swaggerUi from "swagger-ui-express";
 
 import "express-async-errors";
 
+import upload from "@config/upload";
 import { AppError } from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
 
@@ -18,6 +19,9 @@ createConnection("localhost").then(() => console.log("📦 Database connected.")
 
 const app = express();
 app.use(express.json());
+
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
