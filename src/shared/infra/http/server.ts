584f8e2fd2f1cd07b02e6552e1ccf5602472f774
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-12-13 18:00:12 -0300

    Show url that is set in .env to show where the server is running

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,5 @@
 import { app } from "./app";
 
 app.listen(3333, () =>
-  console.log("🚀 Server is running at http://localhost:3333")
+  console.log(`🚀 Server is running at ${process.env.APP_API_URL}`)
 );
