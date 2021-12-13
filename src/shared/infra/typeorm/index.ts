278230c8de194a3a26619f8827d53f6eb23cbce6
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-12-13 17:55:06 -0300

    Set local as default to database connection

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,10 @@
 import { createConnection, getConnectionOptions, Connection } from "typeorm";
 
-export default async (host = "database"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOption = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOption, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : "database",
       database:
         process.env.NODE_ENV === "test" ? "rentx_test" : defaultOption.database,
     })
