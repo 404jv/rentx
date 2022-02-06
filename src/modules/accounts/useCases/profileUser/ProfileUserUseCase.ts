1d6c8f1e05efbdc2a41868bac12e978870d34ef9
João Victor R. Alves <joaovictorramalho7@gmail.com>
2022-02-06 19:56:24 -0300

    Get user in Redis if exists

--- src/modules/accounts/useCases/profileUser/ProfileUserUseCase.ts
+++ src/modules/accounts/useCases/profileUser/ProfileUserUseCase.ts
@@ -3,6 +3,7 @@ import { inject, injectable } from "tsyringe";
 import { IUserResponseDTO } from "@modules/accounts/dtos/IUserResponseDTO";
 import { UserMapper } from "@modules/accounts/mappers/UserMapper";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { getRedis } from "@shared/infra/redis";
 
 @injectable()
 class ProfileUserUseCase {
@@ -12,7 +13,13 @@ class ProfileUserUseCase {
   ) {}
 
   async execute(id: string): Promise<IUserResponseDTO> {
-    const user = await this.usersRepository.findById(id);
+    const userRedis = await getRedis(`user-${id}`);
+
+    let user = JSON.parse(userRedis as string);
+
+    if (!user) {
+      user = await this.usersRepository.findById(id);
+    }
 
     return UserMapper.toDTO(user);
   }
